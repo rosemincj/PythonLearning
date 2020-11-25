cf858cb3d66149599eed89af9338597ccd596a92
rosemincj <74547073+rosemincj@users.noreply.github.com>
2020-11-25 12:19:55 +0530

    Add files via upload

---
+++ sample_program.py
@@ -0,0 +1,48 @@
+def call_palindrome_fun(user_str):
+    str1 = ""
+    for i in user_str:
+        str1 = i + str1
+    # if user_str == str1:
+    return bool(user_str == str1)
+    # else:
+    # return 'It is not a palindrome'
+
+
+def call_substring_palindrome(user_str):
+    x = []
+    for i in range(len(user_str)):
+        for j in range(i + 2, len(user_str) + 1):
+            x.append(user_str[i:j])
+    # return x
+    sample_dict = {}
+    for i in x:
+        sample_dict[i] = call_palindrome_fun(i)
+    # return sample_dict
+    y = []
+    #for i in sample_dict[i]:
+    if sample_dict[i]:
+        y.append(sample_dict[i])
+    return y
+
+
+def call_occurrence_fun(user_str):
+    sample_dict = {}
+    for i in user_str:
+        sample_dict[i] = user_str.count(i)
+    return sample_dict
+
+
+print("Select Option.")
+print("1.Palindrome check")
+print("2.Substring palindrome check")
+print("3.Occurence of each character")
+user_opt = int(input("Enter Option: "))
+user_str = input("Enter string: ")
+if user_opt == 1:
+    print(call_palindrome_fun(user_str))
+elif user_opt == 2:
+    print(call_substring_palindrome(user_str))
+elif user_opt == 3:
+    print(call_occurrence_fun(user_str))
+else:
+    print("Pick a valid option")
